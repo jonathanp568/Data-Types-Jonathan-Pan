1a32cfcd3b69c0bbfafdd11fdceabbb5958f51a8
jonathanp568 <jonathanpan0510@icloud.com>
2025-09-15 07:17:21 -0700

    a

---
+++ test.py
@@ -0,0 +1,17 @@
+# x = 3
+# y = float(3)
+# print(x,y)
+
+# values = [1,2.23,5,7,2,30,15,9]
+# print(values)
+# for i in values:
+#     print(i)
+# print(values[7])
+
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
